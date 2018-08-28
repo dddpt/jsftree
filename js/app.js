be4a67fbe52584c0fdcab7873d8204d2344fe454
Unknown <didier.louis.dupertuis@gmail.com>
2018-08-28 09:26:28 +0200

    Family tree using base d3
    - handles family trees with multiple parents&children at all nodes correctly
    - basic layout (each generation is horizontally centered) and animation

---
+++ js/app.js
@@ -0,0 +1,163 @@
+
+// set the dimensions and margins of the diagram
+var margin = {top: 0, right: 0, bottom: 0, left: 0},
+width = 1300 - margin.left - margin.right,
+height = 1000 - margin.top - margin.bottom;
+
+var transitionsDuration = 500 // duration of transitions in msec
+
+// global variables (mainly used for DEBUGGING)
+var ftreeData = 0
+var ftree = 0
+var nodes = 0
+var links = 0
+var svgg = 0
+
+
+function createFamilyTree(svgContainerSelector,ftreeData){
+  // append the svg obgect to the body of the page
+  // appends a 'group' element to 'svg'
+  // moves the 'group' element to the top left margin
+  var svg = d3.select(svgContainerSelector).append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  svgg = svg.append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+  
+
+  return updateFamilyTree(ftreeData);
+}
+
+function updateFamilyTree(ftreeData, source){
+  //good page for collapsible tree: https://bl.ocks.org/d3noob/43a860bc0024792f8803bba8ca0d5ecd
+
+  ftree = new fTreeBuilder(ftreeData,{width:100,height:100},{width:50,height:50})
+
+  if(!source){
+    source = {x:ftree.width/2,y:0}
+  }
+
+  // maps the node data to the tree layout
+  nodes = ftree.nodes()
+  links = ftree.links()
+
+
+  // adds the links between the nodes
+  var link = svgg.selectAll(".link")
+    .data( links)
+  var linkEnter = link.enter().append("path")
+    .attr("class", d => "link "+d.id+"-"+d.target.id)
+    .attr("d", "M" + source.x + "," + source.y
+        + "C" + source.x + "," + source.y
+        + " " + source.x + "," +  source.y
+        + " " + source.x + "," + source.y
+      )
+      .attr("fill","none")
+      .attr("stroke","lightgrey")
+
+  var linkUpdate = linkEnter.merge(link)
+  linkUpdate.transition()
+    .duration(transitionsDuration)
+    .attr("d", d=> 
+      "M" + d.source.x + "," + d.source.y
+        + "C" + d.source.x + "," + (d.source.y + d.target.y) / 2
+        + " " + d.target.x + "," +  (d.source.y + d.target.y) / 2
+        + " " + d.target.x + "," + d.target.y
+      )
+
+  // Remove any exiting links
+  var linkExit = link.exit().transition()
+      .duration(transitionsDuration)
+      .attr("d", d=> "M" + d.target.x + "," + d.target.y
+        + "C" + d.target.x + "," + d.target.y
+        + " " + d.target.x + "," +  d.target.y
+        + " " + d.target.x + "," + d.target.y
+      )
+      .remove();
+
+  // adds each node as a group
+  var node = svgg.selectAll(".node")
+    .data(nodes.filter(n=>!n.hidden))
+  
+  var nodeEnter = node.enter().append("g")
+    .attr("id", d => "node-"+d.id )
+    .attr("transform", "translate(" + source.x + "," + source.y + ")");
+    //.attr("transform", function(d) { 
+    //  return "translate(" + d.x + "," + d.y + ")"; });
+  
+  // adds the circle to the node
+  nodeEnter.append("circle")
+    .attr("r", d=>d.nodeSize.width/2)
+    .attr("fill","lightblue")
+    .attr("class", d => "node "+d.class )
+
+  // adds the logo -> doesn't work yet
+  nodeEnter.append("foreignObject")
+    .attr("x","-50px")
+    .attr("y","-50px")
+    .attr("width","100px")
+    .attr("height","100px")
+    .append("div")
+    .attr("id",d=> "node-div-"+node.id)
+    .attr("style","height:100px;width:100px;border-radius: 100%;")
+    .append("i")
+    .attr("class",d=>{
+      var logo = "fas fa-"
+      switch(d.class){
+        case "man":
+        logo += "mars"
+        break;
+        case "woman":
+        logo += "venus"
+        break;
+        case "add-node":
+        logo += "user-plus"
+        break;
+        case "add-node":
+        default:
+        logo += "user"
+      }
+      return logo+" icon";
+    })
+  /*node.append("text")
+    .attr("x",0)
+    .attr("y",0)
+    .attr("class","icon")
+    .text(d => {switch(d.class){
+        case "add-node":
+        return("&#xf234;") // fa-user-plus
+        default:
+        return("&#xf007;") // fa-user icon
+      }}
+    )*/
+  
+
+  // adds the text to the node
+  nodeEnter.append("text")
+  .attr("dy", ".35em")
+  .attr("y", 20)
+  .style("text-anchor", "middle")
+  .text(function(d) { return d.name; });
+
+  nodeUpdate = nodeEnter.merge(node)
+  nodeUpdate.transition()
+    .duration(transitionsDuration)
+    .attr("transform", d => "translate(" + d.x + "," + d.y + ")");
+
+  // Remove any exiting nodes
+  var nodeExit = node.exit().transition()
+      .duration(transitionsDuration)
+      .attr("transform", d=> "translate(" + d.links[0].x + "," + d.links[0].y + ")")
+      .remove();
+
+}
+
+
+$.getJSON( "basic_family.json", function( treeData ) {
+  ftreeData = treeData
+
+  createFamilyTree("#graph",ftreeData)
+
+
+});
--- js/ftree.js
+++ js/ftree.js
@@ -1,165 +1,161 @@
+var gdepths = 0
 
 
-var ftree = 0
-$.getJSON( "data/empty_canvas_tree.json", function( treeData ) {
-
-  /*treeData = [{
-      "name": "Niclas Superlongsurname",
-      "class": "man",
-      "textClass": "emphasis",
-      "marriages": [{
-        "spouse": {
-          "name": "Iliana",
-          "class": "woman",
-          "extra": {
-              "nickname": "Illi"
-          }
-        },
-        "children": [{
-          "name": "James",
-          "class": "man",
-          "marriages": [{
-            "spouse": {
-              "name": "Alexandra",
-              "class": "woman"
-            },
-            "children": [{
-              "name": "Eric",
-              "class": "man",
-              "marriages": [{
-                "spouse": {
-                  "name": "Eva",
-                  "class": "woman"
-                }
-              }]
-            }, {
-              "name": "Jane",
-              "class": "woman"
-            }, {
-              "name": "Jasper",
-              "class": "man"
-            }, {
-              "name": "Emma",
-              "class": "woman"
-            }, {
-              "name": "Julia",
-              "class": "woman"
-            }, {
-              "name": "Jessica",
-              "class": "woman"
-            }]
-          },
-          {
-            "spouse": {
-              "name": "Beatriz",
-              "class": "woman"
-            },
-            "children": [{
-              "name": "Zolange",
-              "class": "woman"
-            },{
-              "name": "Zalicia",
-              "class": "woman"
-            }]
-          },
-          {
-            "spouse": {
-              "name": "claricia",
-              "class": "woman"
-            },
-            "children": [{
-              "name": "Colange",
-              "class": "woman"
-            },{
-              "name": "Calicia",
-              "class": "woman"
-            }]
-          }]
-        }]
-      }]
-  }]*/
-
-  /*function circleNodeRenderer(name, x, y, height, width, extra, id, nodeClass, textClass, textRenderer) {
-  //<foreignObject x="-110px" y="48px" width="100px" height="24px" id="1"><circle r="20" class="man" id="node1">
-  //<p class="emphasis" align="center">Niclas Superlongsurname</p></circle></foreignObject>
-    r=20
-
-    var node = '';
-    node += '<circle r=20 '//+ height/2+">";
-    node += 'cx="' + (x+r/2) + '" ';
-    node += 'cx="' + (y+r/2) + '" ';
-    node += 'class="' + nodeClass + '" ';
-    node += 'id="node' + id + '">\n';
-    node += textRenderer(name, extra, textClass);
-    node += '</circle>';
-    return node;
-  }*/
-
-  function circleNodeRenderer(name, x, y, height, width, extra, id, nodeClass, textClass, textRenderer) {
-    var node = '';
-    node += '<div ';
-    node += 'style="height:100px;width:100px;border-radius: 100%;" ';
-    node += 'class="' + nodeClass + '" ';
-    node += 'id="node' + id + '">\n';
-    var logo = ""
-    switch(nodeClass){
-      case "man":
-      logo = "mars"
-      break;
-      case "woman":
-      logo = "venus"
-      break;
-      case "add-node":
-      logo = "user-plus"
-      break;
-      case "add-node":
-      default:
-      logo = "user"
+class fTreeBuilder{
+  constructor(data, nodeSize, nodeSeparation){
+    /*
+    data is an array containing data for a family-tree:
+    - eldest family tree root must be the first element
+    - each element is a family member and its descendance, filled depth first
+      -> the first element contains all the descendance of the eldest family tree root
+    a family member is of the form
+    {
+      name: "name",
+      id:"id",
+      marriages:[
+        {
+          spouse:{spouse_family_member},
+          children:[
+            {child1},
+            {child2},
+            {childi...}
+          ]
+        }
+      ]
     }
-    node += '<i class="fas fa-' + logo + ' sex-symbol"></i>';
-    node += textRenderer(name, extra, textClass);
-    node += '</div>';
-    return node;
+
+    */
+
+    this.nodeSize = nodeSize  // object with width and height value
+    this.nodeSeparation = nodeSeparation // object with width and height value
+    this.flatNodes = {} // object id->node
+    this.depths = {} // object node_id->depth of node
+    this.depths[data[0].id] = 0 // initialize depth of root node
+    this.width = 0 // maximum width needed among the row
+
+    // add the whole family tree sequentially 
+    data.map((node,i) => this.addSubtree(node,undefined,undefined,this.nodeSize,false))
+    this._computeLayout()
   }
 
-  function circleNodeSize(nodes, width, textRenderer){
-    var maxWidth = 0;
-    var maxHeight = 0;
-    var tmpSvg = document.createElement('svg');
-    document.body.appendChild(tmpSvg);
+  // computes depth of given node, has no side effects
+  _computeDepth(node){
+    console.log("_computeDepth for node: "+node.id)
+    if(node.id in this.depths){
+      return(this.depths[node.id])
+    }
+    if(node.marriages){
+      let spouses_depth = node.marriages.map( m => this.depths[m.spouse.id]).filter(d=>d!=undefined)[0]
+      if(spouses_depth!=undefined){return(spouses_depth)}
 
-    _.map(nodes, function (n) {
-      if (n.data.hidden) {
-        n.cWidth = 0;
-        n.cWidth = 0;
-      } else {
-        n.cWidth = width+2;
-        n.cHeight = width+2;
+      let childrenNodes = _.flatten(node.marriages.map( m => m.children))
+      for(let i in childrenNodes){
+        let cdepth = this._computeDepth(childrenNodes[i])
+        if(cdepth!=undefined) return(cdepth-1)
       }
-    });
-    document.body.removeChild(tmpSvg);
-
-    return [width, width];
+    }
+    return(0) // if depth not found: depth is level 0
   }
 
+  _computeLayout(){
+    //let elementsPerRow = _.countBy(this.depths)
+    //let rowWidth = elementsPerRow * (this.nodeSize.width + this.nodeSeparation.width) - this.nodeSeparation.width
+    let nodesPerRow = _.groupBy(this.flatNodes, n => n.depth)
+    //let rowsWidths = Object.keys(nodesPerRow).map(r => _.sumBy(nodesPerRow[r],v=>v.nodeSize.width+this.nodeSeparation.width))
+    let rowsWidths = _.map(nodesPerRow,r => _.sumBy(r,v=>v.nodeSize.width+this.nodeSeparation.width))
+    rowsWidths = rowsWidths.map(rw=>rw-this.nodeSeparation.width) // delete last useless separation
+    this.width = _.max(rowsWidths)
+    console.log("nodesPerRow:")
+    console.log(nodesPerRow)
+    console.log("rowsWidths:")
+    console.log(rowsWidths)
+    console.log("this.width "+this.width)
+    _.map(nodesPerRow,(row,depth) => {
+      console.log("ROW "+depth)
+      let xpos = (this.width-rowsWidths[depth])/2
+      let ypos = depth*(this.nodeSize.height+this.nodeSeparation.height) + this.nodeSize.height/2
+      for(let i in row){
+        console.log("id: "+row[i].id+", xpos: "+xpos+", ypos: "+ypos)
+        xpos += row[i].nodeSize.width/2
+        row[i].y = ypos
+        row[i].x = xpos
+        xpos += row[i].nodeSize.width/2 + this.nodeSeparation.width
+      }
+      console.log("=====")
+    })
 
-  ftree = dTree.init(treeData, {
-    target: "#graph",
-    debug: true,
-    height: 800,
-    width: 1200,
-    callbacks: {
-      nodeClick: function(name, extra) {
-        console.log(name);
-      },
-      textRenderer: function(name, extra, textClass) {
-          if (extra && extra.nickname)
-            name = name + " (" + extra.nickname + ")";
-          return "<p align='center' class='" + textClass + "'>" + name + "</p>";
-      },
-      nodeRenderer: circleNodeRenderer
-      ,nodeSize:circleNodeSize
+  }
+
+  // Function adding the subtree starting at node to flatNodes
+  // - if no depth given, computes it using _computeDepth
+  // - note that if a node is only updated (versus added), addSubTree(updatedNode, ...) will do nothing
+  addSubtree(node, linkedNode, depth, nodeSize,hidden){
+    // if node already flatNodes: exit immediately 
+    if(this.flatNodes[node.id]){
+      node = this.flatNodes[node.id]
     }
-  });
 
-});
+    console.log("addSubtree for node: "+node.id+" with depth "+depth)
+    // add depth
+    if(depth==undefined){
+      depth = this._computeDepth(node)
+      console.log("depth found: "+depth)
+    }
+    node.depth=depth
+    this.depths[node.id] = depth
+    // add node
+    if(!node.linkedNodes){
+      node.linkedNodes=linkedNode?[linkedNode]:[]
+    }
+    else{
+      node.linkedNodes.push(linkedNode)
+    }
+    node.nodeSize=nodeSize
+    node.hidden=hidden
+    this.flatNodes[node.id] = node
+
+    //add marriages, spouse & children nodes
+    if(node.spouse){      
+      node.spouse = this.addSubtree(node.spouse, node, depth, this.nodeSize,false)
+    }
+    if(node.marriages){
+      node.marriages = node.marriages.map( m => {
+        m.id=[node.id,m.spouse.id].sort().toString()
+        return(this.addSubtree(m,node,depth,{width:0,height:0},true))
+      })
+    }
+    if(node.children){
+      node.children.map( c => this.addSubtree(c, node, depth+1, this.nodeSize,false))
+    }
+
+    return(node)
+  }
+
+  nodes(){
+    return(_.values(this.flatNodes))
+  }
+
+  links(){
+    return(_.flatten(_.values(this.flatNodes).map(n=> n.linkedNodes.map(p=>
+      {return({
+      source:{
+        id:n.id,
+        x:n.x,
+        y:n.y,
+      },
+      target:{
+        id:p.id,
+        x:p.x,
+        y:p.y
+      }
+    })
+      }))))
+    }
+}
+
+const d3ftree={
+  init: function(data){
+    return(new fTreeBuilder(data))
+
+  },
+}
