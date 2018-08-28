f4d0b688519f8c0b7f20182cf173cede0cf199ed
Unknown <didier.louis.dupertuis@gmail.com>
2018-08-28 09:20:21 +0200

    Basic dTree family tree
    With this, discovered that dTree only handles regular tree-graphs -> not good enough

--- data/tree_format.js
+++ data/tree_format.js
@@ -3,13 +3,16 @@
   "class": "node",                          // The CSS class of the node
   "textClass": "nodeText",                  // The CSS class of the text in the node
   "depthOffset": 1,                         // Generational height offset
-  "marriages": [{                           // Marriages is a list of nodes
-    "spouse": {                             // Each marriage has one spouse
-      "name": "Mother",
+  "extra": {},                              // Custom data passed to renderers
+  "marriages": [
+    {                           // Marriages is a list of nodes
+      "spouse": {                             // Each marriage has one spouse
+        "name": "Mother",
+      },
+      "children": [{                          // List of children nodes
+        "name": "Child"
+      }]
     },
-    "children": [{                          // List of children nodes
-      "name": "Child"
-    }]
-  }],
-  "extra": {}                               // Custom data passed to renderers
+    {/*other marriage...*/}
+  ]
 }]
--- js/ftree.js
+++ js/ftree.js
@@ -1,143 +1,165 @@
 
-treeData = [{
-    "name": "Niclas Superlongsurname",
-    "class": "man",
-    "textClass": "emphasis",
-    "marriages": [{
-      "spouse": {
-        "name": "Iliana",
-        "class": "woman",
-        "extra": {
-            "nickname": "Illi"
-        }
-      },
-      "children": [{
-        "name": "James",
-        "class": "man",
-        "marriages": [{
-          "spouse": {
-            "name": "Alexandra",
-            "class": "woman"
-          },
-          "children": [{
-            "name": "Eric",
-            "class": "man",
-            "marriages": [{
-              "spouse": {
-                "name": "Eva",
-                "class": "woman"
-              }
+
+var ftree = 0
+$.getJSON( "data/empty_canvas_tree.json", function( treeData ) {
+
+  /*treeData = [{
+      "name": "Niclas Superlongsurname",
+      "class": "man",
+      "textClass": "emphasis",
+      "marriages": [{
+        "spouse": {
+          "name": "Iliana",
+          "class": "woman",
+          "extra": {
+              "nickname": "Illi"
+          }
+        },
+        "children": [{
+          "name": "James",
+          "class": "man",
+          "marriages": [{
+            "spouse": {
+              "name": "Alexandra",
+              "class": "woman"
+            },
+            "children": [{
+              "name": "Eric",
+              "class": "man",
+              "marriages": [{
+                "spouse": {
+                  "name": "Eva",
+                  "class": "woman"
+                }
+              }]
+            }, {
+              "name": "Jane",
+              "class": "woman"
+            }, {
+              "name": "Jasper",
+              "class": "man"
+            }, {
+              "name": "Emma",
+              "class": "woman"
+            }, {
+              "name": "Julia",
+              "class": "woman"
+            }, {
+              "name": "Jessica",
+              "class": "woman"
             }]
-          }, {
-            "name": "Jane",
-            "class": "woman"
-          }, {
-            "name": "Jasper",
-            "class": "man"
-          }, {
-            "name": "Emma",
-            "class": "woman"
-          }, {
-            "name": "Julia",
-            "class": "woman"
-          }, {
-            "name": "Jessica",
-            "class": "woman"
-          }]
-        },
-        {
-          "spouse": {
-            "name": "Beatriz",
-            "class": "woman"
           },
-          "children": [{
-            "name": "Zolange",
-            "class": "woman"
-          },{
-            "name": "Zalicia",
-            "class": "woman"
-          }]
-        },
-        {
-          "spouse": {
-            "name": "claricia",
-            "class": "woman"
+          {
+            "spouse": {
+              "name": "Beatriz",
+              "class": "woman"
+            },
+            "children": [{
+              "name": "Zolange",
+              "class": "woman"
+            },{
+              "name": "Zalicia",
+              "class": "woman"
+            }]
           },
-          "children": [{
-            "name": "Colange",
-            "class": "woman"
-          },{
-            "name": "Calicia",
-            "class": "woman"
+          {
+            "spouse": {
+              "name": "claricia",
+              "class": "woman"
+            },
+            "children": [{
+              "name": "Colange",
+              "class": "woman"
+            },{
+              "name": "Calicia",
+              "class": "woman"
+            }]
           }]
         }]
       }]
-    }]
-}]
+  }]*/
 
-/*function circleNodeRenderer(name, x, y, height, width, extra, id, nodeClass, textClass, textRenderer) {
-//<foreignObject x="-110px" y="48px" width="100px" height="24px" id="1"><circle r="20" class="man" id="node1">
-//<p class="emphasis" align="center">Niclas Superlongsurname</p></circle></foreignObject>
-  r=20
+  /*function circleNodeRenderer(name, x, y, height, width, extra, id, nodeClass, textClass, textRenderer) {
+  //<foreignObject x="-110px" y="48px" width="100px" height="24px" id="1"><circle r="20" class="man" id="node1">
+  //<p class="emphasis" align="center">Niclas Superlongsurname</p></circle></foreignObject>
+    r=20
 
-  var node = '';
-  node += '<circle r=20 '//+ height/2+">";
-  node += 'cx="' + (x+r/2) + '" ';
-  node += 'cx="' + (y+r/2) + '" ';
-  node += 'class="' + nodeClass + '" ';
-  node += 'id="node' + id + '">\n';
-  node += textRenderer(name, extra, textClass);
-  node += '</circle>';
-  return node;
-}*/
+    var node = '';
+    node += '<circle r=20 '//+ height/2+">";
+    node += 'cx="' + (x+r/2) + '" ';
+    node += 'cx="' + (y+r/2) + '" ';
+    node += 'class="' + nodeClass + '" ';
+    node += 'id="node' + id + '">\n';
+    node += textRenderer(name, extra, textClass);
+    node += '</circle>';
+    return node;
+  }*/
 
-function circleNodeRenderer(name, x, y, height, width, extra, id, nodeClass, textClass, textRenderer) {
-  var node = '';
-  node += '<div ';
-  node += 'style="height:100px;width:100px;border-radius: 100%;" ';
-  node += 'class="' + nodeClass + '" ';
-  node += 'id="node' + id + '">\n';
-  node += '<i class="fas fa-' + (nodeClass=="man"? "mars": "venus") + ' sex-symbol"></i>';
-  node += textRenderer(name, extra, textClass);
-  node += '</div>';
-  return node;
-}
+  function circleNodeRenderer(name, x, y, height, width, extra, id, nodeClass, textClass, textRenderer) {
+    var node = '';
+    node += '<div ';
+    node += 'style="height:100px;width:100px;border-radius: 100%;" ';
+    node += 'class="' + nodeClass + '" ';
+    node += 'id="node' + id + '">\n';
+    var logo = ""
+    switch(nodeClass){
+      case "man":
+      logo = "mars"
+      break;
+      case "woman":
+      logo = "venus"
+      break;
+      case "add-node":
+      logo = "user-plus"
+      break;
+      case "add-node":
+      default:
+      logo = "user"
+    }
+    node += '<i class="fas fa-' + logo + ' sex-symbol"></i>';
+    node += textRenderer(name, extra, textClass);
+    node += '</div>';
+    return node;
+  }
 
-function circleNodeSize(nodes, width, textRenderer){
-  var maxWidth = 0;
-  var maxHeight = 0;
-  var tmpSvg = document.createElement('svg');
-  document.body.appendChild(tmpSvg);
+  function circleNodeSize(nodes, width, textRenderer){
+    var maxWidth = 0;
+    var maxHeight = 0;
+    var tmpSvg = document.createElement('svg');
+    document.body.appendChild(tmpSvg);
 
-  _.map(nodes, function (n) {
-    if (n.data.hidden) {
-      n.cWidth = 0;
-      n.cWidth = 0;
-    } else {
-      n.cWidth = width+2;
-      n.cHeight = width+2;
+    _.map(nodes, function (n) {
+      if (n.data.hidden) {
+        n.cWidth = 0;
+        n.cWidth = 0;
+      } else {
+        n.cWidth = width+2;
+        n.cHeight = width+2;
+      }
+    });
+    document.body.removeChild(tmpSvg);
+
+    return [width, width];
+  }
+
+
+  ftree = dTree.init(treeData, {
+    target: "#graph",
+    debug: true,
+    height: 800,
+    width: 1200,
+    callbacks: {
+      nodeClick: function(name, extra) {
+        console.log(name);
+      },
+      textRenderer: function(name, extra, textClass) {
+          if (extra && extra.nickname)
+            name = name + " (" + extra.nickname + ")";
+          return "<p align='center' class='" + textClass + "'>" + name + "</p>";
+      },
+      nodeRenderer: circleNodeRenderer
+      ,nodeSize:circleNodeSize
     }
   });
-  document.body.removeChild(tmpSvg);
 
-  return [width, width];
-}
-
-dTree.init(treeData, {
-  target: "#graph",
-  debug: true,
-  height: 800,
-  width: 1200,
-  callbacks: {
-    nodeClick: function(name, extra) {
-      console.log(name);
-    },
-    textRenderer: function(name, extra, textClass) {
-        if (extra && extra.nickname)
-          name = name + " (" + extra.nickname + ")";
-        return "<p align='center' class='" + textClass + "'>" + name + "</p>";
-    },
-    nodeRenderer: circleNodeRenderer
-    ,nodeSize:circleNodeSize
-  }
 });
